696ae66d42b67578513123eed239665cbd5d86e6
joarder97 <masudul.pallab99@gmail.com>
2023-08-08 12:24:16 +0600

    eth mainnet network config

--- fund-me/script/HelperConfig.s.sol
+++ fund-me/script/HelperConfig.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol"; 
 
 contract HelperConfig {
-    NetworkConfig public acticeNetworkConfig;
+    NetworkConfig public activeNetworkConfig;
 
     struct NetworkConfig {
         address priceFeed;
@@ -14,13 +14,15 @@ contract HelperConfig {
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
+        } else if(block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthonfig();
         } else {
             activeNetworkConfig = getAnvilEthConfig();
         }
     }
 
-    function getSepoliaEthonfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = Netwokonfig(
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig(
             {
                 priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
             }
@@ -28,7 +30,16 @@ contract HelperConfig {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure {
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        
+    }
 
+    function getMainnetEthonfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig(
+            {
+                priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+            }
+        );
+        return ethConfig;
     }
 }
