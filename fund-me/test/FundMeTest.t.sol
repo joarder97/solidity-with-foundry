63c3209eee2b23a15dc556f32bf23f0d9af97561
joarder97 <masudul.pallab99@gmail.com>
2023-08-12 11:01:38 +0600

    gas optimization

--- fund-me/test/FundMeTest.t.sol
+++ fund-me/test/FundMeTest.t.sol
@@ -13,6 +13,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -67,12 +68,16 @@ contract FundMeTest is Test {
         uint256 startingFundMeBalance = address(fundMe).balance;
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
 
-        // vm.txGasPrice(GAS_PRICE);
+        // uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
         // uint256 gasStart = gasleft();
         // // Act
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        // uint256 gasEnd = gasleft();
         vm.stopPrank();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log("gasUsed:", gasUsed);
 
         // uint256 gasEnd = gasleft();
         // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
@@ -90,6 +95,7 @@ contract FundMeTest is Test {
     function testWithDrawFromMultipleFunders() public funded {
         uint160 numberOfFunders = 10;
         uint160 startingFunderIndex = 1;
+
         for (uint160 i = startingFunderIndex; i < numberOfFunders + startingFunderIndex; i++) {
             // we get hoax from stdcheats
             // prank + deal
@@ -102,6 +108,29 @@ contract FundMeTest is Test {
 
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+        assert((numberOfFunders + 1) * SEND_VALUE == fundMe.getOwner().balance - startingOwnerBalance);
+    }
+
+    function testWithDrawFromMultipleFundersheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders + startingFunderIndex; i++) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), STARTING_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithwaw();
         vm.stopPrank();
 
         assert(address(fundMe).balance == 0);
