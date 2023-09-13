9e49f22701fbdc15b0881b048efa9a8a2d8f10ed
joarder97 <masudul.pallab99@gmail.com>
2023-09-13 18:14:23 +0600

    updated

--- nft/src/MoodNft.sol
+++ nft/src/MoodNft.sol
@@ -6,6 +6,9 @@ import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC72
 import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    //errors
+    error MoodNft_CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -30,7 +33,15 @@ contract MoodNft is ERC721 {
     }
 
     function flipMood(uint256 tokenId) public {
-        
+        if(!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert MoodNft_CantFlipMoodIfNotOwner();
+        }
+
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY){
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
